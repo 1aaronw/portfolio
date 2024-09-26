fea4d155d282c3edd9c0ec5b486cb6584e2f57dc
Aaron <aaronwanglucky@gmail.com>
2024-09-26 00:18:14 -0700

    Fixed pfp and title metadata

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ const Home = () => {
   return <div className = "container mx-auto align-items-center">
       <Card>
           <CardHeader className = "flex items-center text-center">
-            <Image className="inline-block align-middle h-36 w-36 rounded-full object-none object-[50%_-80px]"src= {Me} alt="Aaron Icon" />
+            <Image className="inline-block align-middle h-36 w-36"src= {Me} alt="Aaron Icon" />
             <CardTitle className = "text-4xl">Hi, I'm Aaron!</CardTitle>
             <CardContent> I am a junior year Computer Science student and I've always had a passion for computing and technology. Outside of coding, I love solving puzzles, discovering new music, and gaming. Feel free to check out my past projects or reach out to me via any of the contact links on my page!</CardContent>
           </CardHeader>
